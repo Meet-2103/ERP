d37ad50d6ab8ed65ac441ba8d415580fedaf4699
tanvichn <tanvichn@gmail.com>
2024-11-19 21:56:04 +0530

    Add .env

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+
+
 
 const app = express();
+dotenv.config();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/' , (req , res) => {
     res.send("Home Page");
 })
 
-app.listen(PORT);
+app.listen(PORT , () => {console.log(`PORT : ${PORT}`)});
